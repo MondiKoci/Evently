9aab21ba6239c18355b6f6289058df755e1fc263
MondiKoci <Mondi.s.koci@gmail.com>
2020-11-23 19:50:01 -0500

    Add project files.

---
+++ Evently/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Evently
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
